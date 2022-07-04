e9670c47e01fa5b476a5c4abc6a7c0c327dd76b3
Denis Daletski <daletskidenis@gmail.com>
2022-07-05 00:49:54 +0200

    uniform_voronoi: blazing fast uniform sampler

--- uniform_voronoi/main.cpp
+++ uniform_voronoi/main.cpp
@@ -1,21 +1,44 @@
+#include <chrono>
 #include <iostream>
+#include <string>
 
-#include "uniform_sampler.hpp"
+using namespace std::string_literals;
+
 #include <opencv2/opencv.hpp>
+#include "uniform_sampler.hpp"
 
-int main(int argc, char* argv[]) {
-    cv::Mat canvas = cv::Mat::zeros({640, 640}, CV_8UC1);
-    auto sampler = Sampler(canvas.size(), 20);
+int main(int argc, char* argv[])
+{
+    if (argc > 1 && argv[1] == "visualize"s) {
+        auto W = 1920;
+        auto H = 1080;
 
-    std::cout << "started" << std::endl;
-    for (auto sample = sampler.sample(); sample.has_value(); sample = sampler.sample())
-    {
-        std::cout << "got sample: " << sample.value() << std::endl;
-        cv::circle(canvas, sample.value(), 3, {255}, -1);
-        cv::imshow("main", canvas);
-        cv::waitKey(20);
+        cv::Mat canvas = cv::Mat::zeros({W, H}, CV_8UC1);
+        auto sampler = Sampler(canvas.size(), 20);
+
+        for (auto sample = sampler.sample(); sample.has_value(); sample = sampler.sample()) {
+            cv::circle(canvas, sample.value(), 3, {255}, -1);
+            cv::imshow("main", canvas);
+            cv::waitKey(1);
+        }
+        cv::waitKey();
+    } else {
+        auto W = 4096;
+        auto H = 4096;
+
+        cv::Mat canvas = cv::Mat::zeros({W, H}, CV_8UC1);
+        auto sampler = Sampler(canvas.size(), 20);
+
+        auto count = 0;
+        auto start = std::chrono::high_resolution_clock::now();
+        for (auto sample = sampler.sample(); sample.has_value(); sample = sampler.sample()) {
+            ++count;
+        }
+        auto stop = std::chrono::high_resolution_clock::now();
+        auto spent = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start).count();
+        std::cout << "took: " << spent << "ms." << std::endl;
+        std::cout << "points: " << count << std::endl;
     }
-    std::cout << "finished" << std::endl;
 
     return 0;
 }
--- uniform_voronoi/uniform_sampler.hpp
+++ uniform_voronoi/uniform_sampler.hpp
@@ -1,18 +1,21 @@
 #pragma once
 
-#include <vector>
-#include <queue>
 #include <optional>
+#include <queue>
+#include <random>
+#include <vector>
 
+#include <boost/random.hpp>
 #include <opencv2/opencv.hpp>
 
 class Sampler {
-public:
+ public:
     Sampler(cv::Size size, float radius)
-    : _radius(radius)
-    , _radius2(radius * radius)
-    , _cellSize(radius * sqrt(0.5f))
-    , _extent({0, 0}, size)
+        : _radius(radius)
+        , _radius2(radius * radius)
+        , _ringThickness(radius / sqrt(19))
+        , _cellSize(radius * sqrt(0.5f))
+        , _extent({0, 0}, size)
     {
         _gridSize = cv::Size(ceil(size.width / _cellSize), ceil(size.height / _cellSize));
         _gridValues.resize(_gridSize.area());
@@ -21,17 +24,20 @@ public:
         addSample(size / 2);
     }
 
-    std::optional<cv::Point> sample() {
+    std::optional<cv::Point> sample()
+    {
         while (!_queue.empty()) {
-            size_t parentIdx = rand() % _queue.size();
+            size_t parentIdx = _rand() % _queue.size();
             const auto parent = _queue[parentIdx];
-            float seed = 1.0f * rand() / RAND_MAX;
-            float epsilon = 0.0000001f;
+
+            float seed = 1.0 * _rand() / RAND_MAX;
 
             // Make a new candidate.
             for (auto j = 0; j < K; ++j) {
-                float angle = 2 * M_PI * (seed + 1.0f*j/K);
-                float r = _radius + epsilon;
+                auto r = _radius + 1.0 * _ringThickness * _rand() / RAND_MAX;
+
+                float angle = 2 * M_PI * (seed + 1.0f * j / K);
+
                 int x = parent.x + r * cos(angle);
                 int y = parent.y + r * sin(angle);
                 auto candidate = cv::Point(x, y);
@@ -52,10 +58,8 @@ public:
         return std::nullopt;
     }
 
-
-
-private:
-    const int K = 4;
+ private:
+    const int K = 8;
 
     std::vector<cv::Point> _gridValues;
     std::vector<bool> _gridTaken;
@@ -66,16 +70,25 @@ private:
     cv::Rect _extent;
     float _radius;
     float _radius2;
+    float _ringThickness;
 
-    void addSample(cv::Point sample) {
-        size_t gridPos = _gridSize.width * int(sample.y / _cellSize) + int(sample.x / _cellSize);
+    std::uniform_real_distribution<float> _distr01;
+    boost::random::taus88 _rand;
+    // std::minstd_rand0 _rand;
+
+    void addSample(cv::Point sample)
+    {
+        int gridY = sample.y / _cellSize;
+        int gridX = sample.x / _cellSize;
+        size_t gridPos = gridY * _gridSize.width + gridX;
 
         _gridTaken[gridPos] = true;
         _gridValues[gridPos] = sample;
         _queue.push_back(sample);
     }
 
-    bool farFromOtherPoints(cv::Point point) {
+    bool farFromOtherPoints(cv::Point point)
+    {
         int cellY = point.y / _cellSize;
         int cellX = point.x / _cellSize;
         int yMin = std::max(cellY - 2, 0);
@@ -90,8 +103,8 @@ private:
                 auto cellTaken = _gridTaken[gridPos];
                 if (cellTaken) {
                     auto cellValue = _gridValues[gridPos];
-                    int dx = cellValue.x - point.x;
-                    int dy = cellValue.y - point.y;
+                    auto dx = cellValue.x - point.x;
+                    auto dy = cellValue.y - point.y;
                     if (dx * dx + dy * dy < _radius2) {
                         return false;
                     }
@@ -101,73 +114,3 @@ private:
         return true;
     }
 };
-/*
-
-function* poissonDiscSampler(width, height, radius) {
-  const k = 4; // maximum number of samples before rejection
-  const radius2 = radius * radius;
-  const cellSize = radius * Math.SQRT1_2;
-  const gridWidth = Math.ceil(width / cellSize);
-  const gridHeight = Math.ceil(height / cellSize);
-  const grid = new Array(gridWidth * gridHeight);
-  const queue = [];
-
-  // Pick the first sample.
-  yield {add: sample(width / 2 , height / 2, null)};
-
-  // Pick a random existing sample from the queue.
-  pick: while (queue.length) {
-    const i = Math.random() * queue.length | 0;
-    const parent = queue[i];
-    const seed = Math.random();
-    const epsilon = 0.0000001;
-
-    // Make a new candidate.
-    for (let j = 0; j < k; ++j) {
-      const a = 2 * Math.PI * (seed + 1.0*j/k);
-      const r = radius + epsilon;
-      const x = parent[0] + r * Math.cos(a);
-      const y = parent[1] + r * Math.sin(a);
-
-      // Accept candidates that are inside the allowed extent
-      // and farther than 2 * radius to all existing samples.
-      if (0 <= x && x < width && 0 <= y && y < height && far(x, y)) {
-        yield {add: sample(x, y), parent};
-        continue pick;
-      }
-    }
-
-    // If none of k candidates were accepted, remove it from the queue.
-    const r = queue.pop();
-    if (i < queue.length) queue[i] = r;
-    yield {remove: parent};
-  }
-
-  function far(x, y) {
-    const i = x / cellSize | 0;
-    const j = y / cellSize | 0;
-    const i0 = Math.max(i - 2, 0);
-    const j0 = Math.max(j - 2, 0);
-    const i1 = Math.min(i + 3, gridWidth);
-    const j1 = Math.min(j + 3, gridHeight);
-    for (let j = j0; j < j1; ++j) {
-      const o = j * gridWidth;
-      for (let i = i0; i < i1; ++i) {
-        const s = grid[o + i];
-        if (s) {
-          const dx = s[0] - x;
-          const dy = s[1] - y;
-          if (dx * dx + dy * dy < radius2) return false;
-        }
-      }
-    }
-    return true;
-  }
-
-  function sample(x, y, parent) {
-    const s = grid[gridWidth * (y / cellSize | 0) + (x / cellSize | 0)] = [x, y];
-    queue.push(s);
-    return s;
-  }
-}
-*/
